b3185428fbc78fad1733a81a4054ffda5a6075f6
Musab <mohammad.musab.dev@gmail.com>
2026-05-14 22:42:43 +0600

    Create day4_not_operator.py

---
+++ day4_not_operator.py
@@ -0,0 +1,15 @@
+morning = True
+is_evening = not morning
+print(is_evening)
+
+available = True
+print(not available)
+
+open_slot = True
+not_available = not open_slot
+
+print(not True)
+
+allow_duplicates = not True
+
+result = not True
