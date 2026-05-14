b767909aef8f66065130bbc5ee502ea152fe4933
Musab <mohammad.musab.dev@gmail.com>
2026-05-14 16:57:07 +0600

    Create day3_variables.py

---
+++ day3_variables.py
@@ -0,0 +1,21 @@
+# Day 3: Variables in Python
+
+# Create variable auto_save and store True
+auto_save = True
+
+# Show the status of auto-update setting
+print("Auto update")
+print(False)
+
+# Pick value for user unsubscribed from service
+subscribed = False
+
+# Create variable for preventing logout and store False
+prevent_logout = False
+
+# Store variable for calculations with True
+calculate_average = True
+
+# Print values to check
+print(prevent_logout)
+print(calculate_average)
