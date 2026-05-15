e45599f8aa7873b480b8b06a2d467159d7bf043c
Musab <mohammad.musab.dev@gmail.com>
2026-05-15 22:33:12 +0600

    Create day5_equality_operator.py

---
+++ day5_equality_operator.py
@@ -0,0 +1,17 @@
+entered_pin = 5448
+expected_pin = 5440
+print(entered_pin == expected_pin)
+
+print(10 == 10)
+print(9 == 10)
+print(100 == 100)
+
+votes = 10
+print(votes == 11)
+
+print(5 + 13)
+print(5 == 13)
+
+print(10 == 11)
+print(99 == 100)
+print(10 == 13)
