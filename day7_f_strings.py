c1a87dbb0ddda05c009d6177087aab9818abb9c5
Musab <mohammad.musab.dev@gmail.com>
2026-05-17 23:46:54 +0600

    Create day7_f_strings.py

---
+++ day7_f_strings.py
@@ -0,0 +1,12 @@
+print(f"popularity increased by {12}%")
+
+print(f"{4} dataset entries")
+
+print(f"I would walk {500} miles")
+
+degrees = 70
+print(f"Temperature: {degrees} F")
+
+movie = "Vertigo"
+display = f"Airing tonight: {movie}"
+print(display)
